312662326135a2aaedf39de0b524631469d7083f
Mphomo0 <mphomoipolai1@gmail.com>
2026-06-09 19:21:58 +0200

    feat: define recordCountAggregate TableAggregate instance

---
+++ convex/aggregate.ts
@@ -0,0 +1,13 @@
+import { TableAggregate } from "@convex-dev/aggregate";
+import { components } from "./_generated/api";
+import type { DataModel } from "./_generated/dataModel";
+
+export const recordCountAggregate = new TableAggregate<{
+  Namespace: string;
+  Key: number;
+  DataModel: DataModel;
+  TableName: "records";
+}>((components as any).recordCounts, {
+  namespace: (doc) => doc.category,
+  sortKey: (doc) => doc._creationTime,
+});
